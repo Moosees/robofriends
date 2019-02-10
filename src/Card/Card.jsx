551fc37d1bd06952f89622c390f2eb3b018971fc
Moosees <linus.almgren@gmail.com>
2019-02-10 23:20:27 +0100

    Update Card component with props

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Card.css';
 
-const Card = () => (
+const Card = ({ id, name, email }) => (
 	<div className="card">
-		<img src="https://robohash.org/JaneDoe?size=200x200" alt="Jane Doe" />
+		<img src={`https://robohash.org/${id}?size=200x200`} alt="Jane Doe" />
 		<div>
-			<h2>Jane Doe</h2>
-			<p>jane.doe@email.com</p>
+			<h2>{name}</h2>
+			<p>{email}</p>
 		</div>
 	</div>
 );
