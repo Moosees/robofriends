5dab44cbe5015297d7630ed541666b3f084558a8
Moosees <linus.almgren@gmail.com>
2019-02-10 19:09:02 +0100

    Add card component

--- src/Card/Card.jsx
+++ src/Card/Card.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'react';
+import './Card.css';
 
-const Card = () => {
-  return (
-    <div>
-      <p>Card</p>
-    </div>
-  )
-}
+const Card = () => (
+	<div className="card">
+		<img src="https://robohash.org/JaneDoe?size=200x200" alt="Jane Doe" />
+		<div>
+			<h2>Jane Doe</h2>
+			<p>jane.doe@email.com</p>
+		</div>
+	</div>
+);
 
-export default Card
+export default Card;
