edee86c723b2b52eddb449a6a8c8a1aa484d0238
Moosees <linus.almgren@gmail.com>
2019-02-15 21:58:22 +0100

    Fix background on Scroll component

--- src/containers/Scroll.jsx
+++ src/containers/Scroll.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
-const Scroll = (props) => (
-  <div style={{overflowY: 'scroll', border: '5px solid #333', height: '70vh'}}>
-    {props.children}
-  </div>
-);
+const style = {
+	overflowY: 'scroll',
+	border: '5px solid #333',
+	height: '70vh',
+	width: '100%',
+	backgroundColor: '#109e99'
+};
+
+const Scroll = (props) => <div style={style}>{props.children}</div>;
 
 export default Scroll;
